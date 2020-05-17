019ca1b0dd966288ffa60785fbe70555d73a5a4c
Антон Щур <antonschur29@gmail.com>
2020-05-17 18:18:28 +0300

    add setvector function and constructor to multiplication

--- MRZV1/multiplication.cpp
+++ MRZV1/multiplication.cpp
@@ -1,1 +1,33 @@
 #include "multiplication.h"
+
+multiplication::multiplication() {
+	this->setvector(1);
+	this->setvector(2);
+}
+
+void multiplication::setvector(int numofvector) {
+	bool is_end = false;
+	do {
+		string contin;
+		int a;
+		cout << "write number for " << numofvector << " massive\n";
+		cin >> a;
+		if (numofvector == 1) {
+			this->firstvect.push_back(a);
+		}
+		else if (numofvector == 2) {
+			this->secondvect.push_back(a);
+		}
+		while (contin != "y" || contin != "n") {
+			cout << "continue? y/n"; 
+			getline(cin, contin);
+			if (contin == "y") {
+				is_end = false;
+			}
+			else if (contin == "n") {
+				is_end = true;
+			}
+			system("cls");
+		}
+	} while (!is_end);
+}
--- MRZV1/multiplication.h
+++ MRZV1/multiplication.h
@@ -1,5 +1,32 @@
 #pragma once
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <bitset>
+#include <string>
+#include <chrono>
+#include "timer.h"
+
+using namespace std;
+
 class multiplication
 {
+private:
+	mutex mx1;
+	mutex mx2;
+	mutex mx3;
+	mutex mx4;
+
+	int Takt;
+
+	vector<long> firstvect;
+	vector<long> secondvect;
+	vector<long> resultvect;
+
+	void setvector(int);
+public:
+	multiplication();
 };
 
